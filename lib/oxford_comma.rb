9cbe7590cae57372fc1bf91886deb10cfc5b98ba
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-03-20 02:31:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,21 @@ def oxford_comma(array)
   array.join()
     elsif array.length == 2
     array.join(" and ")
-      elsif array.length == 3
-      a.insert(2, "and")
-      array.join(" , ")
+      elsif array.length >= 3
+      array[-1] = "and " << array[-1]
+      array.join(", ")
   end
 end
+
+
+# def oxford_comma(array)
+#   if array.length == 1 
+#   array.join()
+#     elsif array.length == 2
+#     array.join(" and ")
+#       elsif array.length == 3
+#       new_string = ""
+#       new_string << array.join(", ")
+#       new_string.insert(14, "and ")
+#   end
+# end
