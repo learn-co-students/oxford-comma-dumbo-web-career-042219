620509c069509ee7f1904909f385bf9004bd10c7
Joseph G Syverson <josephgavinsyverson@gmail.com>
2019-04-04 20:07:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    first_pt = array[0...-1]
+    second_pt = ", and " + array[-1]
+    first_pt.join(", ") + second_pt
+  end
 end
