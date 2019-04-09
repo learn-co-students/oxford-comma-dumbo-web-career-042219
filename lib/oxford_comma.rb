5d5871238dca4a6426fc436ba0750b13e200c22e
Kevin W <kcwang06@gmail.com>
2019-04-09 22:06:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+     array.join
+   elsif array.length == 2
+     array.join(" and ")
+   else
+     first_part = array[0...-1]
+     second_part = ", and " + array[-1]
+     first_part.join(", ") + second_part
+   end
 end
